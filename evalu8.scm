ba22ef14a94ef43b0f4e227632d21a18633b75d4
1sun12 <regan_sun12@outlook.com>
2024-03-11 15:39:40 -0600

    done with part 1. 'evalu8' function calls are made at the bottom of the code-file to test it

--- evalu8.scm
+++ evalu8.scm
@@ -1,4 +1,41 @@
-(define (add-numbers a b)
-  (+ a b))
 
-(display (add-numbers 2 10)) ; Outputs: 12
+; (define (name param, param1))         ~ creates a function called, "name", with two parameters, param and param1
+; (cond (list? expression))             ~ creates a "if" statement, aka, "cond", that checks if the expression is a list
+
+(define (evalu8 expression bindings)
+  (cond
+    ((list? expression)
+     (case (car expression)
+       ((+)
+        (+ (evalu8 (cadr expression) bindings)
+           (evalu8 (caddr expression) bindings)))
+       ((*)
+        (* (evalu8 (cadr expression) bindings)
+           (evalu8 (caddr expression) bindings)))
+       ((equal?)
+        (equal? (evalu8 (cadr expression) bindings)
+                (evalu8 (caddr expression) bindings)))
+       ((less?)
+        (< (evalu8 (cadr expression) bindings)
+           (evalu8 (caddr expression) bindings)))
+       (else
+        (error "Unknown operation"))))
+    ((number? expression) expression)
+    ((symbol? expression)
+     (let ((value (assoc expression bindings)))
+       (if value
+           (cdr value)
+           (error (string-append (symbol->string expression) " is not bound")))))
+    (else
+     (error "Invalid expression"))))
+
+;; Test environment
+(define env '((a . 12) (b . 5) (c . 7)))
+
+;; Test cases
+(display (evalu8 '(+ a 5) env))  ; Output: 17
+(newline)
+(display (evalu8 '(* (+ a 5) 3) env))  ; Output: 51
+(newline)
+(display (evalu8 '(* (+ a 5) c) env))  ; Output: no output, will error. Error code appears to be '119' and will output that.
+(newline)
