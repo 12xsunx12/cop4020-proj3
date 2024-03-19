38e3c7bf68526353497be1af32dd9082c6eb0d60
Regan O'Donnell <rro6@students.uwf.edu>
2024-03-19 14:14:56 -0600

    making 'c' error

--- evalu8.scm
+++ evalu8.scm
@@ -1,6 +1,11 @@
 
-; (define (name param, param1))         ~ creates a function called, "name", with two parameters, param and param1
-; (cond (list? expression))             ~ creates a "if" statement, aka, "cond", that checks if the expression is a list
+;; (define (name param, param1))         ~ creates a function called, "name", with two parameters, param and param1
+;; (cond (list? expression))             ~ creates a "if" statement, aka, "cond", that checks if the expression is a list
+;; (case (expression))                   ~ a lot like a switch statement
+;;      ->                               ~ for the case (+),        add the two expression and binding environment together
+;;      ->                               ~ for the case (*),        multiply the two expression and binding environment together
+;;      ->                               ~ for the case (equal?),   check if the expression and the binding environment are equal to each other
+;;      ->                               ~ for the case (less?),    check if the expression and the binding environment are inequalities
 
 (define (evalu8 expression bindings)
   (cond
@@ -30,12 +35,14 @@
      (error "Invalid expression"))))
 
 ;; Test environment
-(define env '((a . 12) (b . 5) (c . 7)))
+(define env '((a . 12) (b . 5)
+(c . 7)  ;; comment out to error
+))
 
 ;; Test cases
 (display (evalu8 '(+ a 5) env))  ; Output: 17
 (newline)
 (display (evalu8 '(* (+ a 5) 3) env))  ; Output: 51
 (newline)
-(display (evalu8 '(* (+ a 5) c) env))  ; Output: no output, will error. Error code appears to be '119' and will output that.
+(display (evalu8 '(* (+ a 5) c) env))  ; Output: error, or, if done correctly, will output "119"
 (newline)
